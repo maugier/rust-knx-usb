7bbf06a521adbdc6e78cf4c89f26cede53616d7a
Max <max@xolus.net>
2022-11-16 14:32:27 +0100

    acquire interrupt endpoints addresses

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,48 @@ use anyhow::{anyhow, Result};
 
 trait KNXContext {
     fn find_device<'a>(&'a self) -> Result<Device<'a>>;
+    fn knx(&self) -> Result<KNX>;
 }
 
+trait MyOption {
+    fn none_or<E>(&self, err: E) -> Result<(), E>;
+}
+
+impl<T> MyOption for Option<T> {
+    fn none_or<E>(&self, err: E) -> Result<(), E> {
+        if self.is_none() { Ok(()) } else { Err(err) }
+    }
+}
 
 impl KNXContext for Context {
+
+    fn knx(&self) -> Result<KNX> {
+        let dev = self.find_device()?;
+
+        
+        let config = dev.active_config_descriptor()?;
+        let iface = config.interfaces().next().ok_or(anyhow!("device has no interface"))?;
+        let idesc = iface.descriptors().next().ok_or(anyhow!("interface has no descriptors"))?;
+
+        let mut input = None;
+        let mut output = None;
+
+        for ep in idesc.endpoint_descriptors() {
+            if let libusb::TransferType::Interrupt = ep.transfer_type() {
+                match ep.direction() {
+                    libusb::Direction::In => input.replace(ep.address()).none_or(anyhow!("ambiguous IN endpoints"))?,
+                    libusb::Direction::Out => output.replace(ep.address()).none_or(anyhow!("ambiguous OUT endpoints"))?,
+                }
+            }
+        }
+
+        let input = input.ok_or(anyhow!("no IN endpoint"))?;
+        let output = output.ok_or(anyhow!("no OUT endpoint"))?;
+
+        Ok(KNX { dev, input, output })
+
+    }
+
     fn find_device<'a>(&'a self) -> Result<Device<'a>> {
         for dev in self.devices()?.iter() {
             let dev_desc = dev.device_descriptor()?;
@@ -19,6 +57,12 @@ impl KNXContext for Context {
     }
 }
 
+struct KNX<'a> {
+    dev: Device<'a>,
+    input: u8,
+    output: u8,
+}
+
 fn main() -> Result<()> {
 
     let ctx = Context::new()?;
@@ -26,18 +70,11 @@ fn main() -> Result<()> {
     if !ctx.has_hid_access() {
         eprintln!("WARNING: no hid access");
     }
+
+    let knx = ctx.knx()?;
+
+
     
-    let dev = ctx.find_device()?;
-    let config = dev.active_config_descriptor()?;
-    let iface = config.interfaces().next().ok_or(anyhow!("device has no interface"))?;
-    let idesc = iface.descriptors().next().ok_or(anyhow!("interface has no descriptors"))?;
-
-    eprintln!("Descriptor has {} endpoints", idesc.num_endpoints());
-
-    for ep in idesc.endpoint_descriptors() {
-        eprintln!("endpoint: {:?} {:?} {:?}", ep.direction(), ep.transfer_type(), ep);
-    }
-
     Ok(())
 }
 
